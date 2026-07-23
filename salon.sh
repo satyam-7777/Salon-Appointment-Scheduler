41e822d2542f17a49b18761d9387cb82a37bfa95
Satyam Patel <satyamp9797@gmail.com>
2026-07-23 16:44:05 +0530

    salon sh file

---
+++ salon.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=salon --no-align --tuples-only -c"
+
+echo -e "\n~~~~~ MY SALON ~~~~~\n"
+echo "Welcome to My Salon, how can I help you?"
+
+SERVICES_MENU() {
+  SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
+
+  echo "$SERVICES" | while IFS="|" read SERVICE_ID NAME
+  do
+    echo "$SERVICE_ID) $NAME"
+  done
+}
+
+SERVICES_MENU
+read SERVICE_ID_SELECTED
+
+SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+
+if [[ -z $SERVICE_NAME ]]
+then
+  SERVICES_MENU
+  read SERVICE_ID_SELECTED
+  SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+fi
+
+echo "What's your phone number?"
+read CUSTOMER_PHONE
+
+CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+
+if [[ -z $CUSTOMER_NAME ]]
+then
+  echo "I don't have a record for that phone number, what's your name?"
+  read CUSTOMER_NAME
+
+  $PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')" > /dev/null
+fi
+
+CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+
+echo "What time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
+read SERVICE_TIME
+
+$PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')" > /dev/null
+
+echo "I have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
